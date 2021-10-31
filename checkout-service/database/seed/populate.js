259981fbbf19a7088e9ff4480a0b0930cf435537
Alek <aleksebastian@gmail.com>
2021-10-30 23:10:49 -0500

    Reworded console log

--- checkout-service/database/seed/populate.js
+++ checkout-service/database/seed/populate.js
@@ -1,10 +1,8 @@
-const CreateFakeData = require('./dataGenerator.js');
-const Prinventory = require('../index.js');
-const Mongoose = require('mongoose');
+const CreateFakeData = require("./dataGenerator.js");
+const Prinventory = require("../index.js");
+const Mongoose = require("mongoose");
 
-
-const DbSeed = async function() {
-
+const DbSeed = async function () {
   let checkForPreviousSeedCount = await Prinventory.countDocuments();
   if (checkForPreviousSeedCount) {
     await Prinventory.db.dropDatabase();
@@ -13,17 +11,16 @@ const DbSeed = async function() {
   let dataToSave = await CreateFakeData();
 
   Prinventory.insertMany(dataToSave)
-    .then((result) => console.log(`Database seeded with ${result.length} items`))
-    .catch((err) => console.error('Error seeding database', err))
+    .then((result) =>
+      console.log(`Database seeded with ${result.length} items`)
+    )
+    .catch((err) => console.error("Error seeding database", err))
     .finally(() => {
-      console.log('Mongoose connection closing');
+      console.log("Mongoose connection closing");
       Mongoose.connection.close();
     });
-
 };
 
 DbSeed();
 
 module.exports = DbSeed;
-
-
