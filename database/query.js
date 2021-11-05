b7ab01453b37c587e8047ea8c5ba8c0f84530c86
Alek <aleksebastian@gmail.com>
2021-11-05 17:06:05 -0500

    Created db query for list names and secure transaction text

--- database/query.js
+++ database/query.js
@@ -1,18 +1,33 @@
-const mongoose = require('mongoose');
-const Prinventory = require('./index.js');
+const mongoose = require("mongoose");
+const { Prinventory, ListNamesAndSecureText } = require("./index.js");
 
 const getProductPriceAndInventoryCount = async (id) => {
-  let productPriceAndInventoryCount = await Prinventory.find({id: id}, {"_id": false}).select('id price inventory');
+  let productPriceAndInventoryCount = await Prinventory.find(
+    { id: id },
+    { _id: false }
+  ).select("id price inventory");
   return productPriceAndInventoryCount;
 };
 
 const getMultipleProductsPriceAndInventoryCount = async (ids) => {
-  let productPricesAndInventoryCounts = await Prinventory.find({id: {$in: ids}}, {"_id": false}).select('id price inventory');
+  let productPricesAndInventoryCounts = await Prinventory.find(
+    { id: { $in: ids } },
+    { _id: false }
+  ).select("id price inventory");
   return productPricesAndInventoryCounts;
 };
 
+const getListNamesAndSecureTransactionText = async (id) => {
+  let listNamesAndSecureTransactionText = await ListNamesAndSecureText.find(
+    { id: id },
+    { _id: false }
+  ).select("listNames secureTransactionText");
+  return listNamesAndSecureTransactionText;
+};
 
 module.exports = {
   getProductPriceAndInventoryCount: getProductPriceAndInventoryCount,
-  getMultipleProductsPriceAndInventoryCount: getMultipleProductsPriceAndInventoryCount
+  getMultipleProductsPriceAndInventoryCount:
+    getMultipleProductsPriceAndInventoryCount,
+  getListNamesAndSecureTransactionText: getListNamesAndSecureTransactionText,
 };
