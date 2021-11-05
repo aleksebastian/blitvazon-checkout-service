39f3fb3bbf5ad6611b4edfca30459a74e972610b
Alek <aleksebastian@gmail.com>
2021-11-05 17:03:21 -0500

    Enforced formatting style

--- client/src/components/PriceDeliveryAndStock.jsx
+++ client/src/components/PriceDeliveryAndStock.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Text = styled.span`
-  font-family: "Amazon Ember",Arial,sans-serif;
+  font-family: "Amazon Ember", Arial, sans-serif;
   font-size: 14px;
   display: block;
   line-height: 24px;
@@ -14,7 +14,7 @@ const InlineText = styled(Text)`
 
 const Price = styled(Text)`
   font-size: 18px;
-  color: #B12704;
+  color: #b12704;
 `;
 
 const GreyText = styled(Text)`
@@ -29,8 +29,8 @@ const GreyInlineText = styled(Text)`
 const BlueText = styled(Text)`
   color: #007185;
   cursor: pointer;
-  &:hover{
-    color: #C7511F;
+  &:hover {
+    color: #c7511f;
     text-decoration: underline;
   }
 `;
@@ -39,8 +39,8 @@ const BlueInlineText = styled(Text)`
   color: #007185;
   display: inline;
   cursor: pointer;
-  &:hover{
-    color: #C7511F;
+  &:hover {
+    color: #c7511f;
     text-decoration: underline;
   }
 `;
@@ -53,14 +53,19 @@ const Stock = styled.span`
 `;
 
 const smallerPadding = {
-  lineHeight: "20px"
+  lineHeight: "20px",
 };
 
-
 const PriceDeliveryAndStock = (props) => {
-
   let now = new Date();
-  let night = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0);
+  let night = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate() + 1,
+    0,
+    0,
+    0
+  );
   let minTillMidnight = (night.getTime() - now.getTime()) / 1000 / 60;
   let hours = 0;
   for (let i = 0; i < 24; i++) {
@@ -70,53 +75,73 @@ const PriceDeliveryAndStock = (props) => {
     } else {
       break;
     }
-  };
+  }
 
-  return <div>
+  return (
+    <div style={{ width: "100%" }}>
+      <div style={{ marginBottom: "10px" }}>
+        <Price>
+          {props.price === "Price unavailable"
+            ? "Price unavailable"
+            : `$${props.price}`}
+        </Price>
 
-    <div style={{marginBottom: "10px"}}>
-      <Price>{props.price === 'Price unavailable' ? 'Price unavailable' : `$${props.price}`}</Price>
-
-      {props.isPrimeFreeOneDay && (
-      <div>
-        <img
-          style={{width: "50px", height: "auto"}}
-          src='https://m.media-amazon.com/images/G/01/AmazonServices/Site/US/Product/FBA/small-and-light-prime-logo._V509606070_.png'>
-        </img>
-        <GreyInlineText> FREE One-Day</GreyInlineText>
-        <div>
-          <InlineText>& </InlineText><BlueInlineText>FREE Returns &#8744;</BlueInlineText>
-        </div>
+        {props.isPrimeFreeOneDay && (
+          <div>
+            <img
+              style={{ width: "50px", height: "auto" }}
+              src="https://m.media-amazon.com/images/G/01/AmazonServices/Site/US/Product/FBA/small-and-light-prime-logo._V509606070_.png"
+            ></img>
+            <GreyInlineText> FREE One-Day</GreyInlineText>
+            <div>
+              <InlineText>& </InlineText>
+              <BlueInlineText>FREE Returns &#8744;</BlueInlineText>
+            </div>
+          </div>
+        )}
       </div>
+
+      {props.isFreeDelivery || props.isPrimeFreeOneDay ? (
+        <div style={{ marginBottom: "20px" }}>
+          <Text style={smallerPadding}>
+            FREE delivery: <strong>Tomorrow</strong>
+          </Text>
+          <GreyText style={smallerPadding}>
+            Order within{" "}
+            {hours === 1
+              ? `${hours} hr and ${Math.round(minTillMidnight)} mins`
+              : `${hours} hrs and ${Math.round(minTillMidnight)} mins`}
+          </GreyText>
+          <BlueText style={smallerPadding}>Details</BlueText>
+        </div>
+      ) : (
+        <div style={{ marginBottom: "20px" }}>
+          <Text style={smallerPadding}>
+            Get it in{" "}
+            {`${Math.floor(Number(props.price) / 10)} to ${Math.floor(
+              Number(props.price) / 10 + 2
+            )}`}{" "}
+            days
+          </Text>
+          <GreyText style={smallerPadding}>
+            Shipping & handling: ${`${Number(props.price) / 10}`.slice(0, 4)}
+          </GreyText>
+          <BlueText style={smallerPadding}>Details</BlueText>
+        </div>
       )}
 
+      <Stock
+        style={props.inventory ? { color: "#007600" } : { color: "#B12704" }}
+      >
+        {props.inventory ? "In Stock." : "Currently unavailble."}
+      </Stock>
+      {!props.inventory ? (
+        <Text style={{ lineHeight: "20px" }}>
+          We don't know when this item will be back in stock.
+        </Text>
+      ) : null}
     </div>
-
-    {props.isFreeDelivery || props.isPrimeFreeOneDay ?
-    <div style={{marginBottom: "20px"}}>
-      <Text style={smallerPadding}>FREE delivery: <strong>Tomorrow</strong></Text>
-      <GreyText style={smallerPadding}>
-        Order within {hours === 1 ? `${hours} hr and ${Math.round(minTillMidnight)} mins` : `${hours} hrs and ${Math.round(minTillMidnight)} mins`}
-      </GreyText>
-      <BlueText style={smallerPadding}>Details</BlueText>
-    </div>
-    :
-    <div style={{marginBottom: "20px"}}>
-      <Text style={smallerPadding}>
-        Get it in {`${Math.floor(Number(props.price) / 10)} to ${Math.floor((Number(props.price) / 10) + 2)}`} days
-      </Text>
-      <GreyText style={smallerPadding}>
-        Shipping & handling: ${`${Number(props.price) / 10}`.slice(0, 4)}
-      </GreyText>
-      <BlueText style={smallerPadding}>Details</BlueText>
-    </div>
-    }
-
-    <Stock style={props.inventory ? {color: "#007600"} : {color: "#B12704"}}>{props.inventory ? "In Stock." : "Currently unavailble."}</Stock>
-    {!props.inventory ? <Text style={{lineHeight: "20px"}}>We don't know when this item will be back in stock.</Text> : null}
-
-  </div>
+  );
 };
 
 export default PriceDeliveryAndStock;
-
