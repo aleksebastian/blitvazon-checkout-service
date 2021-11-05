0f3a26d802b618bbbc3029d82a4fdcf86523aa5d
Alek <aleksebastian@gmail.com>
2021-11-05 17:11:41 -0500

    Imported new fake data function and added it into the db seed process

--- database/seed/populate.js
+++ database/seed/populate.js
@@ -1,18 +1,34 @@
-const CreateFakeData = require("./dataGenerator.js");
-const Prinventory = require("../index.js");
+const {
+  createFakeListNamesAndSecureTransactionText,
+  createFakeData,
+} = require("./dataGenerator.js");
+const { Prinventory, ListNamesAndSecureText } = require("../index.js");
 const Mongoose = require("mongoose");
 
 const DbSeed = async function () {
   let checkForPreviousSeedCount = await Prinventory.countDocuments();
+  let checkForPreviousSeedCount2 =
+    await ListNamesAndSecureText.countDocuments();
   if (checkForPreviousSeedCount) {
     await Prinventory.db.dropDatabase();
   }
+  if (checkForPreviousSeedCount2) {
+    await ListNamesAndSecureText.db.dropDatabase();
+  }
 
-  let dataToSave = await CreateFakeData();
+  let dataToSave = await createFakeData();
+  let listNamesAndSecureTextData =
+    await createFakeListNamesAndSecureTransactionText(1);
 
   Prinventory.insertMany(dataToSave)
     .then((result) =>
       console.log(`Database seeded with ${result.length} items`)
+    )
+    .catch((err) => console.error("Error seeding database", err));
+
+  ListNamesAndSecureText.create(listNamesAndSecureTextData)
+    .then((result) =>
+      console.log(`Database seeded with list names and secure text data`)
     )
     .catch((err) => console.error("Error seeding database", err))
     .finally(() => {
@@ -22,5 +38,6 @@ const DbSeed = async function () {
 };
 
 DbSeed();
+createFakeListNamesAndSecureTransactionText();
 
 module.exports = DbSeed;
