8652e3db5eb890bc30936bdb8e2fb336677c73ba
Alek <aleksebastian@gmail.com>
2021-11-05 17:07:10 -0500

    created server route to fetch list names and secure transaction text

--- server/index.js
+++ server/index.js
@@ -5,16 +5,28 @@ const port = 4003;
 const app = express();
 const dbQuery = require("../database/query.js");
 
+// const corsOptions = {
+//   origin: "http://3.142.94.151:3000",
+//   optionsSuccessStatus: 200,
+// };
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/../public"));
 app.use("/:id", express.static(__dirname + "/../public"));
 
-var corsOptions = {
-  origin: "http://localhost:3000",
-  optionsSuccessStatus: 200,
-};
+app.get("/listnamesandsecuretransactiontext/id/:versionId", (req, res) => {
+  dbQuery.getListNamesAndSecureTransactionText(1).then((listNamesAndText) => {
+    if (!listNamesAndText) {
+      res
+        .status(404)
+        .send("Unable to get list names and secure transation text");
+    } else {
+      res.status(200).send(listNamesAndText[0]);
+    }
+  });
+});
 
 app.get("/priceandinventory/id/:productId", (req, res) => {
   let productId = req.params.productId;
